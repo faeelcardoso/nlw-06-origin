35e8b2ceba30cd33dcc7174f9e0cec468b3421ff
Raphael Marques <raphaeljcmarques@gmail.com>
2021-08-18 14:59:18 -0300

    A lot of CSS, two libraries and a package of icons

--- main.js
+++ main.js
@@ -30,3 +30,33 @@ window.addEventListener("scroll", () => {
     header.classList.remove(SHADOW_HEADER);
   }
 });
+
+// Testimonials carousel slider -> swiper lib
+const swiper = new Swiper('.swiper-container', {
+  // todas as propriedades estão na API do swiper
+  slidePerView: 1,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  mousewheel: true, // rodinha do mouse
+  keyboard: true,
+});
+
+// ScrollReveal, mostrar os elementos quando der scroll ná página
+const scrollReveal = ScrollReveal({ // ScrollReveal(options I want to use instead of the defaults)
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true,
+});
+
+// reveal(target, options)
+scrollReveal.reveal(
+  `#home img, #home .text,
+   #about img, #about .text,
+   #services header, #services .card,
+   #testimonials header, #testimonials .testimonials,
+   #contact .text, #contact .links`,
+
+  { interval: 100 },
+);
