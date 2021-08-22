6ab7a0c5691d8b6842d8fc90597d80bb8cf03b47
Raphael Marques <raphaeljcmarques@gmail.com>
2021-08-22 19:35:06 -0300

    footer and a little bit of JS

--- main.js
+++ main.js
@@ -1,62 +1,106 @@
 const MENU = "show";
 const SHADOW_HEADER = "scroll";
+const CONTRAST = "contrast";
 
-const nav = document.querySelector("#mainHeader nav");
-const allToggle = document.querySelectorAll("nav .toggle");
-const links = document.querySelectorAll("#menu ul li a");
+// DOM ELEMENTS
 const header = document.querySelector("#mainHeader");
+const nav = document.querySelector("#mainHeader nav");
+const main = document.querySelector("main");
+const footer = document.querySelector("footer");
+const backToTopButton = document.querySelector(".back-to-top");
+const links = document.querySelectorAll("#menu ul li a");
 
-// adicionando e removendo o menu
-allToggle.forEach(toggle => {
-  toggle.addEventListener("click", () => {
-    nav.classList.toggle(MENU);
-  });
-});
+function toggleMenu() {
+  nav.classList.toggle(MENU);
+}
 
 // removendo o menu quando clicar em alguma opção
-links.forEach(link => {
-  link.addEventListener("click", () => {
-    nav.classList.remove(MENU);
-  });
-});
+function removeMenu() {
+  nav.classList.remove(MENU);
+}
+
+const Libs = {
+  // Testimonials carousel slider -> swiper lib
+  swiper: new Swiper('.swiper-container', {
+      // todas as propriedades estão na API do swiper
+      slidePerView: 1,
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      mousewheel: true, // rodinha do mouse
+      keyboard: true,
+  }),
+
+  //ScrollReveal, mostrar os elementos quando der scroll ná página
+  scrollRevealLib: {
+    scrollReveal: ScrollReveal({ // ScrollReveal(options I want to use instead of the defaults)
+      origin: 'top',
+      distance: '30px',
+      duration: 700,
+      reset: true,
+    }),
+
+    startReveal() {
+      this.scrollReveal.reveal(
+        `#home img, #home .text,
+        #about img, #about .text,
+        #services header, #services .card,
+        #testimonials header, #testimonials .testimonials,
+        #contact .text, #contact .links,
+        footer .brand, footer .social`,
+
+        { interval: 100 },
+      )
+    }
+  }
+}
 
 // sombra no header assim que o scrool tive rolando
-const navHeight = header.offsetHeight; // deslocamento da altura do header, o tamanho do header incluindo borda e padding
+function changeHeader() {
+  const navHeight = header.offsetHeight; // deslocamento da altura do header, o tamanho do header incluindo borda e padding
 
-window.addEventListener("scroll", () => {
   if(window.scrollY > navHeight) { // se o scrool da página passou a altura do header
     header.classList.add(SHADOW_HEADER);
   } else {
     header.classList.remove(SHADOW_HEADER);
   }
-});
+}
 
-// Testimonials carousel slider -> swiper lib
-const swiper = new Swiper('.swiper-container', {
-  // todas as propriedades estão na API do swiper
-  slidePerView: 1,
-  pagination: {
-    el: '.swiper-pagination',
-  },
-  mousewheel: true, // rodinha do mouse
-  keyboard: true,
-});
+// back to top button
+function backToTop() {
+  if(window.scrollY >= 700) {
+    backToTopButton.classList.add(MENU);
+  } else {
+    backToTopButton.classList.remove(MENU);
+  }
+}
 
-// ScrollReveal, mostrar os elementos quando der scroll ná página
-const scrollReveal = ScrollReveal({ // ScrollReveal(options I want to use instead of the defaults)
-  origin: 'top',
-  distance: '30px',
-  duration: 700,
-  reset: true,
-});
+function changeContrastBackToTopButton() {
+  // Mudo a cor do button quando o user chegar no footer
+  const scrollMain = main.scrollHeight;
+  const scrollFooter = footer.scrollHeight;
+  const scrollArrivedInFooter = scrollMain - scrollFooter;
 
-// reveal(target, options)
-scrollReveal.reveal(
-  `#home img, #home .text,
-   #about img, #about .text,
-   #services header, #services .card,
-   #testimonials header, #testimonials .testimonials,
-   #contact .text, #contact .links`,
+ // Workaround mas ficou massa hahahaha
+  if(window.scrollY >= scrollArrivedInFooter) {
+    backToTopButton.classList.add(CONTRAST);
+  } else {
+    backToTopButton.classList.remove(CONTRAST);
+  }
+}
 
-  { interval: 100 },
-);
+/* WHEN SCROLL */
+function scrollEffects() {
+  window.addEventListener("scroll", () => {
+    changeHeader();
+    backToTop();
+    changeContrastBackToTopButton();
+  });
+}
+
+function init() {
+  scrollEffects();
+  Libs.scrollRevealLib.startReveal();
+}
+
+init();
